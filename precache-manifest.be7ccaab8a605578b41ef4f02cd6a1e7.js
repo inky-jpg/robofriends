ab44aeebb90175d9e604d7a15b69dc30b462de3f
pigsyparty <daletruter@gmail.com>
2020-01-06 14:00:09 +0000

    Updates

---
+++ precache-manifest.be7ccaab8a605578b41ef4f02cd6a1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1121ce63889d30fc260d0baf4e2474",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "324f00b4c51657c31354",
+    "url": "/robofriends/static/css/main.37b447b2.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "324f00b4c51657c31354",
+    "url": "/robofriends/static/js/main.e582b217.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
